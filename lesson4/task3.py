e356d0c42ef4300685556515c43c48cce1e300fc
Sergey <barabashka089@gmail.com>
2023-09-20 08:53:54 +0300

    dz1

---
+++ lesson4/task3.py
@@ -0,0 +1,21 @@
+# Задача №29. Решение в группах
+# Ваня:
+# n = int(input())
+# max_number = 1000
+# while n != 0:
+# n = int(input())
+# if max_number > n:
+# max_number = n
+# print(max_number)
+# Петя:
+# n = int(input())
+# max_number = -1
+# while n < 0:
+# n = int(input())
+# if max_number < n:
+# n = max_number
+# print(n)
+
+
+n = int(input())
+max_number = 
