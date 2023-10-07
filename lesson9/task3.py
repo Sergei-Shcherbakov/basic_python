2cc96c4f19a4de4563ed22b1095685d0f211fd0d
Sergey <barabashka089@gmail.com>
2023-10-07 19:39:51 +0300

    dz6

--- lesson9/task3.py
+++ lesson9/task3.py
@@ -13,3 +13,4 @@ data = read_csv('california_housing_test.csv')
 # print(f'{data["median_house_value"].max()},{data["median_house_value"].min()} ')
 # print(data[data['median_income'] == 3.1250]['median_house_value'].max())
 # print(data[data['median_house_value'] == data['median_house_value'].min()]['population'].max())
+ю
